0239760d0471e2fc84a9da419160fbd0d99ff064
Gustavo <dev.gustavo.oliveira@gmail.com>
2022-03-27 05:20:42 +0000

    test to use https on server

--- src/client.js
+++ src/client.js
@@ -1,23 +1,30 @@
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
 const PREFIX = '!';
 const COMMAND = 'deploy';
 
-const DISCORD_TOKEN = '';
+const DISCORD_TOKEN = 'token';
 
-function log(message) {
-  console.log(`${new Date().toISOString()} - ${message}`);
-}
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
 // Add a listener to the client to send a log whatever the client is ready
 client.once('ready', () => {
-  log('Discord connected!');
+  console.log(`${new Date().toISOString()} - cliente.ready: Discord connected!`);
 });
 
-// Add a listener to the client when they send a message and check if the message is a command
-client.on('message', async (message) => {
-  if (message.content.startsWith(`${PREFIX}${COMMAND}`)) {
-    // Send the response to the channel
-    message.channel.send(`${data.shouldideploy ? '👍' : '👎'} ${data.message}`);
-  }
-});
+function message(data) {
+  // Add a listener to the client when they send a message and check if the message is a command
+  client.on('message', async (message) => {
+    if (message.content.startsWith(`${PREFIX}${COMMAND}`)) {
+      // Send the response to the channel
+      message.channel.send(`${data.shouldideploy ? '👍' : '👎'} ${data.message}`);
+    }
+  });
+}
 
 client.login(DISCORD_TOKEN);
+
+module.exports = message;
--- src/server.js
+++ src/server.js
@@ -1,30 +1,24 @@
 const http = require("http");
 const axios = require('axios');
-const Discord = require('discord.js');
-
-const client = new Discord.Client();
+const client = require('./client');
 
 const ROUTE = 'http://shouldideploy.today/api?tz=America/Sao_Paulo';
 
 // Send a request to the server to get the response of "show I deploy today?"
-function getRequest(callback) {
-  axios.get(ROUTE).then((response) => {
-    if (response.data) {
-      callback(response.data);
-    } else {
-      log('No data found');
-    }
-  }).catch((error) => {
-    log(error);
-  });
+async function getData() {
+  const response = await axios.get(ROUTE);
+
+  if (response.status === 200) {
+    return response.data;
+  }
 }
 
-module.exports = http.createServer((request, response) => {
-  getRequest((data) => {
-    // Send the response to Discord chanel
-    require('./client').send(data, client);
-    // Send the response to the client
-    response.writeHead(200, { "Content-Type": "application/json" });
-    response.end(JSON.stringify({ data }));
-  });
+module.exports = http.createServer(async (request, response) => {
+  const data = await getData();
+
+  // Send the response to Discord chanel
+  client(data);
+  // Send the response to the client
+  response.writeHead(200, { "Content-Type": "application/json" });
+  response.end(JSON.stringify({ data }));
 });
