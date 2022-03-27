167287c81bc18475c458e185a655dca3fc3a642b
Gustavo <dev.gustavo.oliveira@gmail.com>
2022-03-27 04:47:40 +0000

    test to use https on server

--- src/client.js
+++ src/client.js
@@ -1,11 +1,6 @@
-const axios = require('axios');
-const Discord = require('discord.js');
-
-const client = new Discord.Client();
-
 const PREFIX = '!';
 const COMMAND = 'deploy';
-const ROUTE = 'http://shouldideploy.today/api?tz=America/Sao_Paulo';
+
 const DISCORD_TOKEN = '';
 
 function log(message) {
@@ -17,31 +12,11 @@ client.once('ready', () => {
   log('Discord connected!');
 });
 
-// Send a request to the server to get the response of "show I deploy today?"
-function getRequest(callback) {
-  axios.get(ROUTE).then((response) => {
-    if (response.data) {
-      callback(response.data);
-    } else {
-      log('No data found');
-    }
-  }).catch((error) => {
-    log(error);
-  });
-}
-
 // Add a listener to the client when they send a message and check if the message is a command
 client.on('message', async (message) => {
   if (message.content.startsWith(`${PREFIX}${COMMAND}`)) {
-    try {
-      getRequest((response) => {
-        // Send the response to the channel
-        message.channel.send(`${response.shouldideploy ? '👍' : '👎'} ${response.message}`);
-      });
-    } catch (err) {
-      // send the error to the channel
-      message.channel.send(`${err.message}`);
-    }
+    // Send the response to the channel
+    message.channel.send(`${data.shouldideploy ? '👍' : '👎'} ${data.message}`);
   }
 });
 
--- src/server.js
+++ src/server.js
@@ -1,8 +1,30 @@
 const http = require("http");
+const axios = require('axios');
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const ROUTE = 'http://shouldideploy.today/api?tz=America/Sao_Paulo';
+
+// Send a request to the server to get the response of "show I deploy today?"
+function getRequest(callback) {
+  axios.get(ROUTE).then((response) => {
+    if (response.data) {
+      callback(response.data);
+    } else {
+      log('No data found');
+    }
+  }).catch((error) => {
+    log(error);
+  });
+}
 
 module.exports = http.createServer((request, response) => {
-  // response.writeHead(200, { "Content-Type": "application/json" });
-  response.writeHead(200, { "Content-Type": "text/html" });
-
-  response.end(JSON.stringify({body: "Hello World!"}));
+  getRequest((data) => {
+    // Send the response to Discord chanel
+    require('./client').send(data, client);
+    // Send the response to the client
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(JSON.stringify({ data }));
+  });
 });
